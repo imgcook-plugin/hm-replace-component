42001e2b6208b58af0ac8b38b5ecc8981bb4075a
胡小根 <hxg@haomo-studio.com>
2020-02-29 08:30:00 +0800

    add error log

--- index.js
+++ index.js
@@ -7,11 +7,12 @@
  */
 
 const pluginHandler = async options => {
-  let { data, filePath, config  } = options;
+  let { data, filePath, config } = options;
   try {
-    console.log(data);
-    console.log(filePath);
-    console.log(config);
+    // console.log(data);
+    // console.log(filePath);
+    // console.log(config);
+    console.log('hm-replace-component');
   } catch(err) {
     console.error('hm-replace-component error: ' + err.toString());
   }
@@ -21,6 +22,7 @@ const pluginHandler = async options => {
 
 module.exports = (...args) => {
   return pluginHandler(...args).catch(err => {
+    console.log('hm-replace-component error');
     console.log(err);
   });
 };
