02cf3b15a334ba51800e907a0c2605e868643831
胡小根 <hxg@haomo-studio.com>
2020-03-02 22:09:14 +0800

    coding

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 const Handlebars = require("handlebars");
-const _ = require('lodash');
+const _ = require("lodash");
 const prettier = require("prettier");
 
-let helpers = require('handlebars-helpers')({
+let helpers = require("handlebars-helpers")({
   handlebars: Handlebars
 });
 
@@ -13,49 +13,144 @@ let projectDir = process.cwd();
 
 function loadJson(jsonFile) {
   let contents = fs.readFileSync(jsonFile);
-  return data = JSON.parse(contents);
+  return (data = JSON.parse(contents));
 }
 
 /**
- * 加载得到组件的 xml，用来替换生成后的代码 
+ * 加载得到组件的 xml，用来替换生成后的代码
  */
 function loadComponentXml(componentUiConfig) {
-  console.log('loadComponentXml: ' + componentUiConfig.component);
+  console.log("loadComponentXml: " + componentUiConfig.component);
   // 加载组件的模板
   let componentTemplateFile = path.join(projectDir, componentUiConfig.template);
   let componentTemplate = fs.readFileSync(componentTemplateFile).toString();
 
   // 替换组件中的props(attr/event): :data-id="options['{{id}}']['id']" 等
   componentTemplate = componentTemplate.replace(
-    /:data-id?=\s?['"]options\[['"]\{\{id\}\}['"]\]\[['"][a-zA-Z0-9]+['"]\]['"]/g, 
-      `data-id='${_.kebabCase(componentUiConfig.component)}'`);
-  componentTemplate = componentTemplate.replace(/\{\{title\}\}/g, `imgcook替换组件：${componentUiConfig.component}`);
+    /:data-id?=\s?['"]options\[['"]\{\{id\}\}['"]\]\[['"][a-zA-Z0-9]+['"]\]['"]/g,
+    `data-id='${_.kebabCase(componentUiConfig.component)}'`
+  );
+  componentTemplate = componentTemplate.replace(
+    /\{\{title\}\}/g,
+    `imgcook替换组件：${componentUiConfig.component}`
+  );
   let attrReg = /:[a-z0-9\-]+\s?=\s?['"]options\[['"]\{\{id\}\}['"]\]\[['"][a-zA-Z0-9]+['"]\]['"]/g;
   let attrNameReg = /:[a-z0-9\-]+/g;
   if (componentTemplate.match(attrReg)) {
     componentTemplate.match(attrReg).forEach(attr => {
-      let attrName = attr.match(attrNameReg)[0].replace(':', '');
-      componentTemplate = componentTemplate.replace(attr, `${attrName}="{{${_.camelCase(attrName)}}}"`)
-    })
+      let attrName = attr.match(attrNameReg)[0].replace(":", "");
+      componentTemplate = componentTemplate.replace(
+        attr,
+        `${attrName}="{{${_.camelCase(attrName)}}}"`
+      );
+    });
   } else {
-    console.log('不存在除data-id外的属性绑定');
+    console.log("不存在除data-id外的属性绑定");
   }
-  
+
   let eventReg = /@[a-z0-9\-]+\s?=\s?['"]options\[['"]\{\{id\}\}['"]\]\[['"][a-zA-Z0-9]+['"]\]['"]/g;
   let eventNameReg = /@[a-z0-9\-]+/g;
   if (componentTemplate.match(eventReg)) {
     componentTemplate.match(eventReg).forEach(event => {
-      let eventName = attr.match(eventNameReg)[0].replace('@', '');
-      componentTemplate = componentTemplate.replace(event, `@${eventName}=\`{{${_.camelCase(eventName)}}}\``);
-    })
+      let eventName = attr.match(eventNameReg)[0].replace("@", "");
+      componentTemplate = componentTemplate.replace(
+        event,
+        `@${eventName}=\`{{${_.camelCase(eventName)}}}\``
+      );
+    });
   } else {
-    console.log('不存在事件绑定');
+    console.log("不存在事件绑定");
   }
-  
+
   // 渲染模板
   componentTemplate = `\n${componentTemplate}\n`; // 加换行
   let template = Handlebars.compile(componentTemplate);
-  return template(Object.assign({title: 'imgcook替换'}, componentUiConfig.defaultProps));
+  return template(
+    Object.assign({ title: "imgcook替换" }, componentUiConfig.defaultProps)
+  );
+}
+
+function replaceComponent(panel, UiConfig) {
+  let keyPattern = /\{\{\s?['"]hm-component=([a-z0-9\-]+)['"]\s?\}\}/g;
+  let componentUiConfigMap = {};
+  if (!panel.panelValue.match(keyPattern)) {
+    return;
+  }
+  panel.panelValue.match(keyPattern).forEach(keyStr => {
+    let component = keyStr
+      .replace(/\{\{\s?['"]hm-component=/g, "")
+      .replace(/['"]\s?\}\}/g, "");
+    let componentUiConfig = UiConfig[component];
+    if (!componentUiConfig) {
+      console.warn(
+        `找不到组件 ${component} 的ui config，请核对文件 ${uiConfigFile}`
+      );
+      return;
+    }
+
+    componentUiConfigMap[component] = componentUiConfig;
+
+    // 渲染得到组件的xml
+    let componentXml = loadComponentXml(componentUiConfig);
+
+    // 替换模板对应的xml
+    panel.panelValue = panel.panelValue.replace(
+      new RegExp(keyStr, "gm"),
+      componentXml
+    );
+  });
+
+  // 对于存在importPath的组件，需要将生成的代码执行引入操作
+  Object.values(componentUiConfigMap).forEach(componentUiConfig => {
+    if (!componentUiConfig.importPath) {
+      return;
+    }
+
+    let importComponentStr = `import ${componentUiConfig.component} from ${componentUiConfig.importPath}`;
+    // 对于vue生成的vue文件和对于jsx的文件(react)，需要引入组件
+    if (panel.panelType == "vue") {
+      // 引入vue组件
+      panel.panelValue.replace(
+        "export default",
+        `${importComponentStr};
+          export default`
+      );
+    } else if (panel.panelType == "jsx") {
+      // 引入react组件
+      panel.panelValue.replace(
+        `'use strict';`,
+        `'use strict';
+          ${importComponentStr};`
+      );
+    }
+  });
+
+  // 对panelValue进行prettier
+  try {
+    if (panel.panelType == "vue") {
+      panel.panelValue = prettier.format(panel.panelValue, {
+        semi: true,
+        tabWidth: 2,
+        parser: "vue"
+      });
+    } else if (panel.panelType == "jsx") {
+      panel.panelValue = prettier.format(panel.panelValue, {
+        semi: true,
+        tabWidth: 2,
+        parser: "babel"
+      });
+    }
+  } catch (err) {
+    console.error(err.stack);
+  }
+}
+
+function replaceCss(panel, UiConfig) {
+  // @TODO: 提取生成后代码的style
+
+  // @TODO: 读取组件style替换映射
+
+  // @TODO: 将生成后的style，替换为目标组件对应的style。
 }
 
 /**
@@ -68,9 +163,9 @@ function loadComponentXml(componentUiConfig) {
 const pluginHandler = async options => {
   let { data, filePath, config } = options;
   try {
-    console.log('hm-replace-component');
+    console.log("hm-replace-component");
     // 读取项目组件的列表。uni-ui.js
-    console.log(process.cwd())
+    console.log(process.cwd());
     let configJsonFile = `${projectDir}/.xmind2code.json`;
     console.log(`读取.xmind2code.json文件: ${configJsonFile}`);
     let xmind2codeJson = loadJson(configJsonFile);
@@ -79,61 +174,12 @@ const pluginHandler = async options => {
     let UiConfig = require(uiConfigFile)();
 
     // 提取模板中的 {{ 'hm-component=van-field' }} 类似的表达式，并用表达式的模板进行替换
-    let keyPattern = /\{\{\s?['"]hm-component=([a-z0-9\-]+)['"]\s?\}\}/g;
     data.code.panelDisplay.forEach(panel => {
-      let componentUiConfigMap = {};
-      if (!panel.panelValue.match(keyPattern)) {
-        return;
-      }
-      panel.panelValue.match(keyPattern).forEach(keyStr => {
-        let component = keyStr.replace(/\{\{\s?['"]hm-component=/g, '').replace(/['"]\s?\}\}/g, '');
-        let componentUiConfig = UiConfig[component];
-        if (!componentUiConfig) {
-          console.warn(`找不到组件 ${component} 的ui config，请核对文件 ${uiConfigFile}`);
-          return;
-        }
-
-        componentUiConfigMap[component] = componentUiConfig;
-
-        // 渲染得到组件的xml
-        let componentXml = loadComponentXml(componentUiConfig);
-
-        // 替换模板对应的xml
-        panel.panelValue = panel.panelValue.replace(new RegExp(keyStr, 'gm'), componentXml);
-      })
-
-      // 对于存在importPath的组件，需要将生成的代码执行引入操作
-      Object.values(componentUiConfigMap).forEach(componentUiConfig => {
-        if (!componentUiConfig.importPath) {
-          return;
-        }
-
-        let importComponentStr = `import ${componentUiConfig.component} from ${componentUiConfig.importPath}`;
-        // 对于vue生成的vue文件和对于jsx的文件(react)，需要引入组件
-        if (panel.panelType == 'vue') {
-          // 引入vue组件
-          panel.panelValue.replace('export default', `${importComponentStr};
-          export default`)
-        } else if (panel.panelType == 'jsx') {
-          // 引入react组件
-          panel.panelValue.replace(`'use strict';`, `'use strict';
-          ${importComponentStr};`)
-        }
-      })
-
-      // 对panelValue进行prettier
-      try {
-        if (panel.panelType == 'vue') {
-          panel.panelValue = prettier.format(panel.panelValue, { semi: true, tabWidth: 2, parser: "vue" });
-        } else if (panel.panelType == 'jsx') {
-          panel.panelValue = prettier.format(panel.panelValue, { semi: true, tabWidth: 2, parser: "babel" });
-        }
-      } catch(err) {
-        console.error(err.stack);
-      }
-    })
-  } catch(err) {
-    console.error('hm-replace-component error: ' + err.toString());
+      replaceComponent(panel, UiConfig);
+      replaceCss(panel, UiConfig);
+    });
+  } catch (err) {
+    console.error("hm-replace-component error: " + err.toString());
     console.error(err.stack);
   }
   return { data, filePath, config };
@@ -141,7 +187,7 @@ const pluginHandler = async options => {
 
 module.exports = (...args) => {
   return pluginHandler(...args).catch(err => {
-    console.log('hm-replace-component error');
+    console.log("hm-replace-component error");
     console.log(err);
   });
 };
